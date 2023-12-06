bb01fccebdb780cdc4fe1ff06432c588b533b93b
Pury <sondongxuan1205@gmail.com>
2023-12-06 23:59:43 +0700

    complete api crawl data league

---
+++ crawl/controller/scrape.js
@@ -0,0 +1,102 @@
+const puppeteer = require("puppeteer");
+
+const scrape = async (url, league) => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  page.on('console', msg => console.log('PAGE LOG:', msg.text()));
+  await page.setDefaultNavigationTimeout(60000);
+  await page.goto(url);
+  await page.waitForSelector(".jstable");
+
+  const data = await page.evaluate(() => {
+    const table = document.querySelector(".jstable");
+    const matchdayElements = table.querySelectorAll(".jstable-row.js-mdname");
+    const dataContent = {
+      matchdays: [],
+    };
+
+    matchdayElements.forEach((matchdayElement) => {
+      const matchdayName = matchdayElement
+          .querySelector(".jsrow-matchday-name")
+          .textContent.trim();
+
+      const matchday = {
+        name: matchdayName,
+        matches: [],
+      };
+
+      let matchWrapperElement = matchdayElement.nextElementSibling;
+
+      while (matchWrapperElement) {
+        if (matchWrapperElement.classList.contains("jstable-row") &&
+            matchWrapperElement.classList.contains("js-mdname")) {
+          break;
+        }
+
+        const matchElementsTitle = matchWrapperElement.querySelectorAll(
+            ".js-matchday-col.js-matchday-title"
+        );
+
+        const matchdayMatchesElements = matchWrapperElement.querySelectorAll(".js-matchday-col.js-matchday-matches")
+
+        matchElementsTitle.forEach((matchElement, index1) => {
+          matchdayMatchesElements.forEach((matchdayMatchesElement, index2) => {
+            if (matchdayMatchesElement[index2] === matchElement[index1]) {
+              const matchDate = matchElement
+                  .querySelector(".js-matchday-date")
+                  .textContent.trim();
+
+              const dataMatches = []
+
+              const jsTableRows = matchdayMatchesElement.querySelectorAll(".jstable-row");
+              jsTableRows.forEach((jsTableRow) => {
+
+                const hours = jsTableRow.querySelector(".jstable-cell.jsMatchDivTime .jsDivLineEmbl").textContent;
+                const jsMaTran = jsTableRow.querySelector(".jstable-cell.js-ma-tran .jsDivLineEmbl").textContent.trim();
+                const jsMatchDivVenue = jsTableRow.querySelector(".jstable-cell.jsMatchDivVenue").textContent.trim();
+                const jsMatchDivHome = jsTableRow.querySelector(".jstable-cell.jsMatchDivHome .jsDivLineEmbl  .js_div_particName a").textContent.trim();
+                const jsMatchDivScore = jsTableRow.querySelector(".jstable-cell.jsMatchDivScore .jsScoreDiv a").textContent.trim();
+                const jsMatchDivAway = jsTableRow.querySelector(".jstable-cell.jsMatchDivAway .jsDivLineEmbl  .js_div_particName a").textContent.trim();
+                const jsChannelDiv = jsTableRow.querySelector(".jstable-cell.jsChannelDiv").textContent.trim();
+                const jsAudience = jsTableRow.querySelector(".jstable-cell.js-audience .jsDivLineEmbl").textContent.trim();
+
+                const dataMatch = {
+                  hours: hours,
+                  maTran: jsMaTran,
+                  venue: jsMatchDivVenue,
+                  home: jsMatchDivHome,
+                  score: jsMatchDivScore,
+                  away: jsMatchDivAway,
+                  channel: jsChannelDiv,
+                  audience: jsAudience
+                }
+
+                dataMatches.push(dataMatch);
+              })
+
+              const match = {
+                dateMatch: matchDate,
+                dataMatches
+              };
+
+              matchday.matches.push(match);
+              matchWrapperElement = matchWrapperElement.nextElementSibling;
+            } else {
+              return;
+            }
+          })
+        });
+      }
+
+      dataContent.matchdays.push(matchday);
+    });
+
+    return dataContent;
+  });
+
+  await browser.close();
+
+  return {status: "success", league: league, data: data};
+};
+
+module.exports = scrape;
---
+++ crawl/routes/crawl.js
@@ -0,0 +1,147 @@
+const express = require("express");
+const router = express.Router();
+const pool = require("../database/dbConnect");
+const {v4: uuidv4} = require('uuid');
+const scrape = require("../controller/scrape");
+
+// Scraping data from NightWolf
+router.post("/nightWolf", async (req, res) => {
+  try {
+    const crawl = await scrape(process.env.CRAWURLNIGHTWOLF, "Night Wolf 2023/24");
+
+    const sqlDelete = "DELETE FROM `match_of_league` WHERE league_id = 1";
+
+    await queryAsync(sqlDelete);
+
+    for (const matchday of crawl.data.matchdays) {
+      for (const match of matchday.matches) {
+        for (const dataMatch of match.dataMatches) {
+          const uuid = uuidv4();
+          const sqlInsert = "INSERT INTO `match_of_league` (`id`,`league_id`, `title`,`date` ,`hour`, `ordinal_number`, `venue`, `home`, `away`, `score`, `channel`, `audience`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+          await queryAsync(sqlInsert, [uuid, 1, matchday.name, match.dateMatch, dataMatch.hours, dataMatch.maTran, dataMatch.venue, dataMatch.home, dataMatch.away, dataMatch.score, dataMatch.channel, dataMatch.audience]);
+        }
+      }
+    }
+
+    return res.status(200).send({message: "Insert data to database success", data: crawl});
+  } catch (error) {
+    return res.status(500).send({message: "Internal Server Error", error});
+  }
+});
+
+// Scraping data from Bia sao vang
+router.post("/biaSaoVang", async (req, res) => {
+  try {
+    const crawl = await scrape(process.env.CRAWURLBIASAOVANG, "Hạng Nhất Quốc Gia Bia Sao Vàng 2023/24");
+    const sqlInsert = "INSERT INTO `match_of_league` (`id`,`league_id`, `title`,`date` ,`hour`, `ordinal_number`, `venue`, `home`, `away`, `score`, `channel`, `audience`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+    const sqlDelete = "DELETE FROM `match_of_league` WHERE league_id = 2";
+
+    await queryAsync(sqlDelete);
+
+    for (const matchday of crawl.data.matchdays) {
+      for (const match of matchday.matches) {
+        for (const dataMatch of match.dataMatches) {
+          const uuid = uuidv4();
+
+          await queryAsync(sqlInsert, [uuid, 2, matchday.name, match.dateMatch, dataMatch.hours, dataMatch.maTran, dataMatch.venue, dataMatch.home, dataMatch.away, dataMatch.score, dataMatch.channel, dataMatch.audience]);
+        }
+      }
+    }
+
+    return res.status(200).send(crawl);
+  } catch (e) {
+    return res.status(500).send({message: "Internal Server Error", e});
+  }
+});
+
+// Scraping data from CASPER
+router.post("/casper", async (req, res) => {
+  try {
+    const crawl = await scrape(process.env.CRAWURLCASPER, "CÚP QUỐC GIA CASPER 2023/24");
+    const sqlInsert = "INSERT INTO `match_of_league` (`id`,`league_id`, `title`,`date` ,`hour`, `ordinal_number`, `venue`, `home`, `away`, `score`, `channel`, `audience`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+    const sqlDelete = "DELETE FROM `match_of_league` WHERE league_id = 3";
+
+    await queryAsync(sqlDelete);
+
+    for (const matchday of crawl.data.matchdays) {
+      for (const match of matchday.matches) {
+        for (const dataMatch of match.dataMatches) {
+          const uuid = uuidv4();
+
+          await queryAsync(sqlInsert, [uuid, 3, matchday.name, match.dateMatch, dataMatch.hours, dataMatch.maTran, dataMatch.venue, dataMatch.home, dataMatch.away, dataMatch.score, dataMatch.channel, dataMatch.audience]);
+        }
+      }
+    }
+
+    return res.status(200).send(crawl);
+  } catch (e) {
+    return res.status(500).send({message: "Internal Server Error", e});
+  }
+});
+
+// Get data of night wolf league
+router.get("/nightWolf", async (req, res) => {
+  try {
+    const sql = "SELECT * FROM `match_of_league` WHERE league_id = 1";
+
+    const data = await queryAsync(sql);
+
+    if (!data) {
+      res.status(404).send({message: "Not found data"});
+    }
+
+    return res.status(200).send(data);
+  } catch (e) {
+    return res.status(500).send({message: "Internal Server Error", e});
+  }
+});
+
+// Get data of biaSaoVang league
+router.get("/biaSaoVang", async (req, res) => {
+  try {
+    const sql = "SELECT * FROM `match_of_league` WHERE league_id = 2";
+
+    const data = await queryAsync(sql);
+
+    if (!data) {
+      res.status(404).send({message: "Not found data"});
+    }
+
+    return res.status(200).send(data);
+  } catch (e) {
+    return res.status(500).send({message: "Internal Server Error", e});
+  }
+});
+
+// Get data of casper league
+router.get("/casper", async (req, res) => {
+  try {
+    const sql = "SELECT * FROM `match_of_league` WHERE league_id = 3";
+
+    const data = await queryAsync(sql);
+
+    if (!data) {
+      res.status(404).send({message: "Not found data"});
+    }
+
+    return res.status(200).send(data);
+  } catch (e) {
+    return res.status(500).send({message: "Internal Server Error", e});
+  }
+});
+
+// Hàm thực hiện truy vấn với async/await
+const queryAsync = (sql, params) => {
+  return new Promise((resolve, reject) => {
+    pool.query(sql, params, (err, result) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(result);
+      }
+    });
+  });
+}
+
+module.exports = router;
