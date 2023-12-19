8bf2250399f4d6c9a20eaf586864d389aeeaf574
hungaa1 <nguyentronghung05072003@gmail.com>
2023-12-19 16:35:23 +0700

    ok

--- crawl/database/dbConnect.js
+++ crawl/database/dbConnect.js
@@ -2,6 +2,7 @@ const mysql = require("mysql2");
 
 const pool = mysql.createPool({
   host: process.env.HOSTDB,
+  port: 3307,
   user: process.env.USERDB,
   database: process.env.DATABASE,
   waitForConnections: true,
--- crawl/index.js
+++ crawl/index.js
@@ -1,10 +1,17 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const crawl = require("./routes/crawl");
 const port = process.env.PORT || 9001;
 
+app.use(
+    cors({
+        credentials: true,
+        origin: [`${ process.env.REACT_URL }`],
+    }),
+);
 app.use("/crawl", crawl);
 
 app.listen(port, () => {
-    console.log(`Example app listening on url "http://localhost:${port}"`);
+    console.log(`Example app listening on url "http://localhost:${ port }"`);
 });
